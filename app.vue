99587c7b058945a0c878e5144034e14db17a2b61
donglingxi <123131683@qq.com>
2022-06-09 19:37:52 +0800

    init
    init

---
+++ app.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div>test</div>
+  </div>
+</template>
+<script setup type="module">
+import { createMachine, assign } from 'xstate'
+
+const increment = context => {
+  console.log('content+', content)
+  return context.count + 1
+}
+const decrement = context => {
+  console.log('content-', content)
+  return context.count - 1
+}
+
+const counterMachine = createMachine({
+  id: 'counter',
+  initial: 'active',
+  context: {
+    count: 0,
+  },
+  states: {
+    active: {
+      on: {
+        INC: { actions: assign({ count: increment }) },
+        DEC: { actions: assign({ count: decrement }) },
+      },
+    },
+  },
+})
+</script>
